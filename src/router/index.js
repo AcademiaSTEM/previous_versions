872d207a83ded1ddd3256868a8c0bd1b17e62bc3
lily416 <daconcha@uc.cl>
2024-01-16 00:28:26 -0300

    feat: manage not existing pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,10 @@
+/* eslint-disable consistent-return */
 import { createRouter, createWebHistory } from 'vue-router';
 
 import MainView from '../views/MainView.vue';
 import ClassView from '../views/ClassView.vue';
+import PageNotFoundView from '../views/PageNotFoundView.vue';
+import info from '../storage/info';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +18,17 @@ const router = createRouter({
       name: 'Class',
       component: ClassView,
       props: true,
+      beforeEnter: (to) => {
+        const data = info[to.params.id];
+        if (typeof data === 'undefined') {
+          return { name: 'PageNotFound' };
+        }
+      },
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'PageNotFound',
+      component: PageNotFoundView,
     },
   ],
 });
---
+++ src/views/PageNotFoundView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="layout-main-container">
+    <div class="layout-main">
+      <header>
+        <h4>Ups! Hubo un error</h4>
+        <h1>Página no encontrada</h1>
+      </header>
+
+      <Divider />
+      <p>Vuelve al inicio para continuar utilizando la página.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PageNotFoundView',
+};
+</script>
