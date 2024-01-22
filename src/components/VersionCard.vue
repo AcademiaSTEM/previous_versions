4b1916ceec8543ea70540b96e4f567b457ee7334
lily416 <daconcha@uc.cl>
2024-01-22 02:42:05 -0300

    feat: add version 2024-s1

---
+++ src/components/VersionCard.vue
@@ -0,0 +1,49 @@
+<template>
+    <Card :pt="style_option">
+      <template #header>
+          <img :src="imageUrl" />
+      </template>
+      <template #title>{{ year }}</template>
+      <template #subtitle>{{ version }}</template>
+      <template #footer>
+        <a :href="versionUrl">
+          <Button
+            type="button"
+            label="Ver versión"
+            icon="pi pi-book"
+            outlined
+          />
+        </a>
+      </template>
+    </Card>
+</template>
+
+<script>
+export default {
+  name: 'VersionCard',
+  props: {
+    info: {
+      type: Object,
+      default: () => {},
+    },
+  },
+  data(props) {
+    const imageUrl = new URL(`../assets/images/${props.info.year}_s${props.info.version_id}.jpg`, import.meta.url).href;
+    let versionUrl = 'https://academiastem.github.io/';
+    if (!props.info.version.includes('actual')) {
+      versionUrl += `${props.info.year}_s${props.info.version_id}`;
+    }
+    return {
+      ...props.info,
+      imageUrl,
+      versionUrl,
+      style_option: {
+        root: { class: 'flex flex-grow-1' },
+        body: { class: 'flex flex-column', style: 'width: 100%' },
+        content: { class: 'flex-grow-1' },
+        footer: { class: 'inline-flex justify-content-center' },
+      },
+    };
+  },
+};
+</script>
--- src/views/PreviousVersionsView.vue
+++ src/views/PreviousVersionsView.vue
@@ -2,18 +2,46 @@
   <div class="layout-main-container">
     <div class="layout-main">
       <header>
-        <h4>Primeros pasos en ingeniería</h4>
-        <h1>Versiones anteriores</h1>
+        <h4>STEM UC: Academia para escolares</h4>
+        <h1>Primeros pasos en programación</h1>
       </header>
 
       <Divider />
+      <h2>Versiones anteriores</h2>
+      <div class="grid align-items-stretch gap-3 px-0 sm:px-5 md:px-8">
+        <VersionCard
+          v-for="info in versions"
+          :info="info"
+          :key="info.id"
+          class="flex col-12 md:col-5" />
+        <div
+         v-if="(versions.length % 2) && (windowWidth >= 576)"
+         class="flex col-0 md:col-5 flex-grow-1"
+        />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import { useWindowSize } from '@vueuse/core';
+import VersionCard from '../components/VersionCard.vue';
+
 export default {
   name: 'PreviousVersionsView',
+  components: {
+    VersionCard,
+  },
+  data() {
+    const { width } = useWindowSize();
+    return {
+      versions: [
+        { year: 2024, version_id: 1, version: 'Primera semana' },
+        { year: 2024, version_id: 2, version: 'Segunda semana (actual)' },
+      ],
+      windowWidth: width,
+    };
+  },
 };
 </script>
 
